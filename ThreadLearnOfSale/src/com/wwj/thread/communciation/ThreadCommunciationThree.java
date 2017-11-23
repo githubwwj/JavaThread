4f1acfa7b1bea20a3b5685bf46b589f40a27fcae
wwj <wwj_computeryes@163.com>
2017-11-23 18:21:30 +0800

    修复错误线程间通信代码

--- ThreadLearnOfSale/src/com/wwj/thread/communciation/ThreadCommunciationThree.java
+++ ThreadLearnOfSale/src/com/wwj/thread/communciation/ThreadCommunciationThree.java
@@ -47,20 +47,28 @@ class Input implements Runnable{  //小卡车输入煤
 				Condition productMonitor=res.getProductMonitor();
 				Condition consumeMonitor=res.getConcumeMonitor();
 				try{
+					
+					if(res.sumCar>99){
+						System.out.println(Thread.currentThread().getName()+"======PPPPPPPPP===== over========");  // 1 bob 退出
+						break;
+					}
+					
 					while(res.flag){
 						try {
 							productMonitor.await();
-//							res.wait();   //bob(睡)   lili(睡)
+//							res.wait();  
 						} catch (InterruptedException e) {
 							e.printStackTrace();
 						}
 					}
 					
-					if(res.sumCar>99){
+					if(res.sumCar==100){
+						res.flag=true;   // 3 lili 退出
+						consumeMonitor.signal();
+						System.out.println(Thread.currentThread().getName()+"======PPPPPPPPP===== over========");
 						break;
 					}
 					
-					try {Thread.sleep(10);} catch (InterruptedException e) {e.printStackTrace();}
 					if(x==0){
 						res.name="精煤";
 						res.weight=6;
@@ -71,7 +79,7 @@ class Input implements Runnable{  //小卡车输入煤
 					x=(x+1)%2;
 					res.flag=true;
 					res.sumCar++;
-					System.out.println(Thread.currentThread().getName()+"=====拉了一车"+res.name+"煤========="+res.sumCar+"车"); // bob 车   lili车
+					System.out.println(Thread.currentThread().getName()+"=====拉了一车"+res.name+"煤========="+res.sumCar+"车"); 
 					consumeMonitor.signal();
 //					res.notify();
 				}finally{
@@ -104,13 +112,17 @@ class Output implements Runnable{  //锅炉烧煤
 				while(!res.flag){
 					try {
 						consumeMonitor.await();
-//						res.wait();  //mike(睡着)  jenny(睡着)
+//						res.wait();  //mike(活)  jenny(睡着) 
 					} catch (InterruptedException e) {
 						e.printStackTrace();
 					}
 				}
+				
+				if(res.name==null){
+					System.out.println(Thread.currentThread().getName()+"========SSSSSSSSSSSSSSSSSSSSSSSSS=== OVER========"); //4 jenny 退出了
+					break;
+				}
 					
-				try {Thread.sleep(10);} catch (InterruptedException e) {e.printStackTrace();}
 				System.out.println(Thread.currentThread().getName()+"=======烧了一车"+res.name+"======"+res.weight+"吨========"+res.sumCar+"车");
 				res.name=null;
 				res.weight=0;
@@ -118,12 +130,13 @@ class Output implements Runnable{  //锅炉烧煤
 //				res.notifyAll();
 				productMonitor.signal();
 				if(res.sumCar>99){
+					System.out.println(Thread.currentThread().getName()+"========SSSSSSSSSSSSSSSSSSSSSSSSS=== OVER========");  //2 mike 退出了
 					break;
 				}
 			}finally{
 				lock.unlock();
 			}
-				
+			//1生产者  2 消费者  3生产者  4 消费者
 //			}
 		}
 	}
@@ -133,7 +146,6 @@ class Output implements Runnable{  //锅炉烧煤
 public class ThreadCommunciationThree {
 
 	public static void main(String[] args) {
-		
 		
 		Resource res=new Resource();  //煤
 		
@@ -152,6 +164,13 @@ public class ThreadCommunciationThree {
 		
 		mike.start();
 		jenny.start();
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		
 		
 
 	}
