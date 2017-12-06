b150554ba2250ae461c40d807ff7e804dd5ee521
wwj <wwj_computeryes@163.com>
2017-12-06 11:58:46 +0800

    新增线程池

--- ThreadLearnOfSale/src/com/wwj/thread/pool/LearnThreadPool.java
+++ ThreadLearnOfSale/src/com/wwj/thread/pool/LearnThreadPool.java
@@ -2,42 +2,56 @@ package com.wwj.thread.pool;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class LearnThreadPool {
-
+	
 	public static void main(String[] args) {
+
+//		createFixedThreadPool();
+		createSingThreadPool();
+		
+	}
+
+	private static void createSingThreadPool() {
+		
+		ScheduledExecutorService scheduledExecutorService=Executors.newSingleThreadScheduledExecutor();
+		
+		scheduledExecutorService.scheduleAtFixedRate(new Runnable() {
+			
+			@Override
+			public void run() {
+				System.out.println(Thread.currentThread().getName()+"==========");
+			}
+		}, 5, 3, TimeUnit.SECONDS);
+		
+	}
+
+	private static void createFixedThreadPool() {
 		
 		ExecutorService executorService=Executors.newFixedThreadPool(3);
 		
-		for(int e=1;e<=10;e++){
-			
+		for(int e=0;e<10;e++){
 			final int index=e;
 			executorService.execute(new Runnable() {
 				
 				@Override
 				public void run() {
-					
-					System.out.println(Thread.currentThread().getName()+"===第"+index+"个任务");
 					try {
-						Thread.sleep(1500);
+						Thread.sleep(2000);
 					} catch (InterruptedException e) {
 						e.printStackTrace();
 					}
+					System.out.println(Thread.currentThread().getName()+"====="+index);
 					
 				}
 			});
-			
 		}
 		
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		System.out.println(Thread.currentThread().getName());
+		
 		executorService.shutdown();
-		
-
+		System.out.println(Thread.currentThread().getName()+"=====over=======");
 	}
 
 }
